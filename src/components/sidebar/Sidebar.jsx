3e63d928e87c82c54580d6d8af9c106c89298c2d
Abrar Haque <abm.abrar@gmail.com>
2023-05-23 23:58:50 +0200

    colorOptions

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -69,6 +69,7 @@ const Sidebar = () => {
             <div className="bottom">
                 <div className="colorOption"></div>
                 <div className="colorOption"></div>
+                <div className="colorOption"></div>
             </div>
         </div>
     );
