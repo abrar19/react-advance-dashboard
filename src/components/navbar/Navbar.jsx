24a81fe9950aa30ba822a8c831285de883ade68c
Abrar Haque <abm.abrar@gmail.com>
2023-05-24 23:18:13 +0200

    Navbar done

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,37 @@
 import React from 'react';
 import './navbar.scss'
+import { ChatBubbleOutline, ChatBubbleOutlineOutlined, DarkModeOutlined, FullscreenExitOutlined, LanguageOutlined, ListOutlined, NotificationsNoneOutlined, SearchOutlined } from '@mui/icons-material';
 
 const Navbar = () => {
     return (
         <div className='navbar'>
-            Navbar
+            <div className="wrapper">
+                <div className="search">
+                    <input type="text" placeholder='Search...' />
+                    <SearchOutlined />
+                </div>
+                <div className="items">
+                    <div className="item">
+                        <LanguageOutlined />
+                        English
+                    </div>
+                    <div className="item">
+                        <DarkModeOutlined />
+                    </div>
+                    <div className="item">
+                        <FullscreenExitOutlined />
+                    </div>
+                    <div className="item">
+                        <NotificationsNoneOutlined />
+                    </div>
+                    <div className="item">
+                        <ChatBubbleOutlineOutlined />
+                    </div>
+                    <div className="item">
+                        <ListOutlined />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -69,7 +69,6 @@ const Sidebar = () => {
             <div className="bottom">
                 <div className="colorOption"></div>
                 <div className="colorOption"></div>
-                <div className="colorOption"></div>
             </div>
         </div>
     );
