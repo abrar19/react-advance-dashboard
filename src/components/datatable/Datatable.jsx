406116adeace095af05f11e1364b582cc511b6d1
Abrar Haque <abm.abrar@gmail.com>
2023-07-03 01:16:40 +0200

    users page with datatable done

---
+++ src/components/datatable/Datatable.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import './datatable.scss'
+import { DataGrid } from '@mui/x-data-grid';
+import { userColumns, userRows } from "../../datatablesource";
+
+const Datatable = () => {
+
+    const actionColumn = [{
+        field: "action",
+        headerName: "Action",
+        width: 200,
+        renderCell: (params) => {
+            return(
+                <div className='cellAction'>
+                    <div className="viewButton">View</div>
+                    <div className="deleteButton">Delete</div>
+                </div>
+            )
+        }
+    }]
+    return (
+        <div className='datatable'>
+            <DataGrid
+                rows={userRows}
+                columns={userColumns.concat(actionColumn)}
+                initialState={{
+                pagination: {
+                    paginationModel: { page: 0, pageSize: 9 },
+                },
+                }}
+                pageSizeOptions={[5, 10]}
+                checkboxSelection
+            />
+        </div>
+    );
+}
+
+export default Datatable;
