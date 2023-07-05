1afd0e67c90adf1578b88b863ac614b63ecfcfb2
Abrar Haque <abm.abrar@gmail.com>
2023-07-05 23:45:11 +0200

    single user page done

--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import './new.scss'
+import Sidebar from '../../components/sidebar/Sidebar';
+import Navbar from '../../components/navbar/Navbar';
 
 const New = () => {
     return (
         <div className='new'>
-            New
+            <Sidebar />
+            <div className="newContainer">
+                <Navbar />
+                test
+            </div>
         </div>
     );
 }
--- src/pages/single/Single.jsx
+++ src/pages/single/Single.jsx
@@ -3,6 +3,7 @@ import './single.scss'
 import Sidebar from '../../components/sidebar/Sidebar'
 import Navbar from '../../components/navbar/Navbar'
 import Chart from '../../components/chart/Chart'
+import List from '../../components/table/Table'
 
 const Single = () => {
     return (
@@ -41,7 +42,10 @@ const Single = () => {
                         <Chart aspect={4/1} title="User Spending(Last 6 Months)"/>
                     </div>
                 </div>
-                <div className="bottom"></div>
+                <div className="bottom">
+                <h1 className="title">Last Transactions</h1>
+                    <List />
+                </div>
             </div>
         </div>
     );
